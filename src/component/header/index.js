8a1a58b583dc085cd370fb8a439e0b80eeaf6d88
karan kiri <karan.kiri@rebelfoods.co>
2020-09-30 18:07:54 +0530

    header component

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
+import { FoodContext } from "../../utils/context/foodContext";
+import "./style.scss";
 
 export default function Header() {
-  return <div>Header</div>;
+  const {
+    user: { user },
+  } = useContext(FoodContext);
+  const cartQuantity = useMemo(() => {
+    return user.cart ? user.cart.count : 0;
+  }, [user]);
+  return (
+    <header className="app-header">
+      <div className="app-logo">
+        <h3>Foody</h3>
+      </div>
+      <div className="cart-container">
+        <div className="icon-container"></div>
+        <span className="quantity">{cartQuantity}</span>
+      </div>
+    </header>
+  );
 }
